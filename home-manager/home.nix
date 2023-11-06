1d1b657b702f6f5912e9b67f751d12310478ce04
Sebastian Klähn <scoreplayer2000@gmail.com>
2023-11-06 22:15:42 +0100

    clean

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,5 +1,3 @@
-# This is your home-manager configuration file
-# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
 {
   inputs,
   lib,
@@ -8,11 +6,7 @@
   outputs,
   ...
 }: {
-  # You can import other home-manager modules here
-  imports = [
-    # If you want to use home-manager modules from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModule
-  ];
+  imports = [];
 
   nixpkgs = {
     overlays = [
