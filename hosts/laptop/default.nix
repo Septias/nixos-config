296543c4bbf833fde2fcd9bba574624bf21028d7
Septias <scoreplayer2000@gmail.comclear>
2023-11-09 16:55:55 +0100

    add hardware conf for laptop

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -1,9 +1,8 @@
 
 { config, pkgs, ... }:
 {
-
+  inputs = [./hardware-configuration.nix];
   networking.hostName = "nixos-laptop";
-
   # Laptop power management
   powerManagement.enable = true;
   powerManagement.powertop.enable = true;
