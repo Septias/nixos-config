32515308b28c37f13692e80582dd1e51bf397824
Septias <scoreplayer2000@gmail.comclear>
2023-12-27 17:43:57 +0100

    remove rust config for helix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -92,19 +92,6 @@
           display-inlay-hints = true;
         };
       };
-      languages = {
-        language = [{
-          name = "rust";
-          config = {
-            inlayHints.bindingModeHints.enable = false;
-            inlayHints.closingBraceHints.minLines = 10;
-            inlayHints.closureReturnTypeHints.enable = "with_block";
-            inlayHints.discriminantHints.enable = "fieldless";
-            inlayHints.lifetimeElisionHints.enable = "skip_trivial";
-            inlayHints.typeHints.hideClosureInitialization = false;
-          };
-        }];
-      };
     };
     vscode = {
       enable = true;
