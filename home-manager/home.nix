3948332e46b176a223fb6a4b9f223feb6471c69a
Septias <scoreplayer2000@gmail.comclear>
2023-12-27 21:00:07 +0100

    catppuccin frappe for alacritty

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -39,13 +39,10 @@
     evince
     libreoffice
     agda
-
     nil
     scc
     fd
-
     rust-analyzer
-
     inputs.dc-times.packages.x86_64-linux.dc-times
   ];
 
@@ -64,6 +61,93 @@
     };
     alacritty = {
       enable = true;
+      settings = {
+        colors = {
+          primary = {
+            background = "#303446";
+            foreground = "#C6D0F5";
+            # Bright and dim foreground colors
+            dim_foreground = "#C6D0F5";
+            bright_foreground = "#C6D0F5";
+          };
+          
+          cursor = {
+            text = "#303446";
+            cursor = "#F2D5CF";
+          };
+
+          vi_mode_cursor = {
+              text = "#303446"; # base
+              cursor = "#BABBF1"; # lavender
+          };
+
+          # Search colors
+          search.matches = {
+              foreground = "#303446"; # base
+              background = "#A5ADCE"; # subtext0
+          };
+
+          search.focused_match = {
+              foreground = "#303446"; # base
+              background = "#A6D189"; # green
+          };
+          footer_bar = {
+              foreground = "#303446"; # base
+              background = "#A5ADCE"; # subtext0
+          };
+          # Keyboard regex hints
+          hints.start = {
+              foreground = "#303446"; # base
+              background = "#E5C890"; # yellow
+          };
+
+          hints.end = {
+              foreground = "#303446"; # base
+              background = "#A5ADCE"; # subtext0
+          };
+          # Selection colors
+          section = {
+              text = "#303446"; # base
+              background = "#F2D5CF"; # rosewater
+          };
+
+          # Normal colors
+          normal = {
+              black = "#51576D"; # surface1
+              red = "#E78284"; # red
+              green = "#A6D189"; # green
+              yellow = "#E5C890"; # yellow
+              blue = "#8CAAEE"; # blue
+              magenta = "#F4B8E4"; # pink
+              cyan = "#81C8BE"; # teal
+          white = "#B5BFE2"; # subtext1
+          };
+
+          # Bright colors
+          bright = {
+              black = "#626880"; # surface2
+              red = "#E78284"; # red
+              green = "#A6D189"; # green
+              yellow = "#E5C890"; # yellow
+              blue = "#8CAAEE"; # blue
+              magenta = "#F4B8E4"; # pink
+              cyan = "#81C8BE"; # teal
+              white = "#A5ADCE"; # subtext0
+          };
+
+          # Dim colors
+          dim = {
+              black = "#51576D"; # surface1
+              red = "#E78284"; # red
+              green = "#A6D189"; # green
+              yellow = "#E5C890"; # yellow
+              blue = "#8CAAEE"; # blue
+              magenta = "#F4B8E4"; # pink
+              cyan = "#81C8BE"; # teal
+              white = "#B5BFE2"; # subtext1
+          };
+        };
+      };
     };
     zsh = {
       enable = true;
