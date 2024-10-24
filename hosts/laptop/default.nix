d072632e638ce48f2eb449bcd700b3a8eb24d353
Septias <scoreplayer2000@gmail.com>
2024-10-24 16:34:00 +0200

    enable touchegg and remove fprintd

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -1,7 +1,9 @@
-{...}: {
+{lib, ...}: {
   imports = [./hardware-configuration.nix];
 
   networking.hostName = "nixos-laptop";
   powerManagement.enable = true;
-  powerManagement.powertop.enable = true;
+  powerManagement.powertop.enable = true;  
+  services.fprintd.enable = lib.mkForce false;
+  services.touchegg.enable = true;
 }
