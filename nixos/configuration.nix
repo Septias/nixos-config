875523005ca3398601c0a45678bf1b026e0dcd08
Septias <scoreplayer2000@gmail.com>
2024-11-27 17:42:53 +0100

    add bluetooth enhancements

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -116,7 +116,14 @@
     jack.enable = true;
   };
 
-  # realtime scheduling for user processes
+  # services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
+  #   "monitor.bluez.properties" = {
+  #       "bluez5.enable-sbc-xq" = true;
+  #       "bluez5.enable-msbc" = true;
+  #       "bluez5.enable-hw-volume" = true;
+  #       "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+  #   };
+  # };  # realtime scheduling for user processes
   security.rtkit.enable = true;
 
   services.minecraft-server = {
