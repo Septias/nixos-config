f3ca6fd28ecc036933ac190885544d13d8c593c5
Septias <scoreplayer2000@gmail.com>
2024-12-27 18:47:11 +0100

    hyprpaper

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -3,7 +3,17 @@
     ./waybar
     ./rofi
   ];
-  services.hyprpaper.enable = true;
+  services.hyprpaper = {
+    enable = true;
+    settings = {  
+      ipc = "on";
+      splash = false;
+      splash_offset = 2.0;
+
+      preload = [ "~/coding/nixos-config/home-manager/mask.jpeg" ];
+      wallpaper = ", ~/coding/nixos-config/home-manager/mask.jpeg";
+    };
+  };
   home.packages = with pkgs; [
     (writeShellScriptBin "screenshot" ''
       grim -g "$(slurp)" - | wl-copy
