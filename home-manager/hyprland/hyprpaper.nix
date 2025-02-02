d9d42ef4f642aa1747a9bac84d2d292a8084f8db
Sebastian Klähn <scoreplayer2000@gmail.com>
2025-02-02 11:18:02 +0100

    change wallpaper

--- home-manager/hyprland/hyprpaper.nix
+++ home-manager/hyprland/hyprpaper.nix
@@ -5,8 +5,8 @@
       ipc = "on";
       splash = false;
       splash_offset = 2.0;
-      preload = ["~/coding/nixos-config/home-manager/mask.jpeg"];
-      wallpaper = ", ~/coding/nixos-config/home-manager/mask.jpeg";
+      preload = ["${../wallpaper.jpeg}"];
+      wallpaper = ", ${../wallpaper.jpeg}";
     };
   };
 }
