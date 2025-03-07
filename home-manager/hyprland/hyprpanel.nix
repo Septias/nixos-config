488738a19425ba99f1acbe6a562ead52b7e64664
Sebastian Klähn <scoreplayer2000@gmail.com>
2025-03-07 08:50:45 +0100

    hyprpanel settings

--- home-manager/hyprland/hyprpanel.nix
+++ home-manager/hyprland/hyprpanel.nix
@@ -1,24 +1,41 @@
-{inputs, ...} : {
-  imports = [ inputs.hyprpanel.homeManagerModules.hyprpanel ];
-  # TODO: Add hyprsunset, kblayout, powermodule, matugen?
+{
+  inputs,
+  config,
+  ...
+}: {
+  imports = [inputs.hyprpanel.homeManagerModules.hyprpanel];
   programs.hyprpanel = {
     enable = true;
     hyprland.enable = true;
     overwrite.enable = true;
-    theme = "nord";
+    theme = "catppuccin_mocha";
     settings = {
-      theme.bar.transparent = true;
+      theme.bar = {
+        transparent = false;
+        enableShadow = true;
+      };
       theme.font = {
         name = "JetBrainsMono Nerd Font";
         size = "12px";
       };
-      bar.clock.format = "%a %b %d %H:%M";
-      bar.launcher.autoDetectIcon = true;
-      menus.clock.weather.location = "Berlin";
-      # menu.clock.weather.unit = "";
-      menus.dashboard.stats.enable_gpu = true;
-      menus.media.noMediaText = "No music ~ uwu";
-      menus.power.confirmation = false;
+      bar = {
+        clock.format = "%a %b %d %H:%M";
+        launcher.autoDetectIcon = true;
+        customModules.hyprsunset.label = false;
+      };
+      menus = {
+        clock = {
+          weather.unit = "metric";
+          weather.location = "Freiburg";
+          time.military = true;
+          weather.key = config.sops.secrets.weather.path;
+        };
+        dashboard.stats.enable_gpu = true;
+        media.noMediaText = "No music ~ uwu";
+        power.confirmation = false;
+        dashboard.directories.enabled = false;
+      };
+      wallpaper.enable = false;
     };
   };
 }
