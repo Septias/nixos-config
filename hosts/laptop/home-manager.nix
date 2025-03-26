4f247ed95fdb44e67f57a8e9749e88fe553e68be
Septias <scoreplayer2000@gmail.com>
2025-03-26 14:42:28 +0100

    add work monitor

--- hosts/laptop/home-manager.nix
+++ hosts/laptop/home-manager.nix
@@ -1,6 +1,7 @@
 {lib, ...}: {
   wayland.windowManager.hyprland.settings.monitor = [
     "eDP-1,1920x1200,0x0,1"
+    "desc:Eizo Nanao Corporation EV2785 0x03015951, 2560x1440, 0x-1440,1"
     ", preferred, auto, 1, mirror, eDP-1"
   ];
   programs.hyprpanel.layout = lib.mkOverride 10 {
