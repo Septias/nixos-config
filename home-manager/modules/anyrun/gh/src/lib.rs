272a253d142f170bd3dc9e9ccdd09d0b2ad372ca
Septias <scoreplayer2000@gmail.com>
2025-04-04 16:10:11 +0200

    create gh plugin

--- home-manager/modules/anyrun/gh/src/lib.rs
+++ home-manager/modules/anyrun/gh/src/lib.rs
@@ -1,37 +1,66 @@
 use abi_stable::std_types::{ROption, RString, RVec};
 use anyrun_plugin::*;
+use serde::Deserialize;
+use std::{fs, process::Command};
+use urlencoding::encode;
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    prefix: String,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            prefix: "gh".to_string(),
+        }
+    }
+}
 
 #[init]
-fn init(config_dir: RString) {
-    // Your initialization code. This is run in another thread.
-    // The return type is the data you want to share between functions
+fn init(config_dir: RString) -> Config {
+    match fs::read_to_string(format!("{}/gh.ron", config_dir)) {
+        Ok(content) => ron::from_str(&content).unwrap_or_default(),
+        Err(_) => Config::default(),
+    }
 }
 
 #[info]
 fn info() -> PluginInfo {
     PluginInfo {
-        name: "Demo".into(),
-        icon: "help-about".into(), // Icon from the icon theme
+        name: "Github Search".into(),
+        icon: "help-about".into(),
     }
 }
 
 #[get_matches]
-fn get_matches(input: RString) -> RVec<Match> {
-    // The logic to get matches from the input text in the `input` argument.
-    // The `data` is a mutable reference to the shared data type later specified.
-    vec![Match {
-        title: "Test match".into(),
-        icon: ROption::RSome("help-about".into()),
-        use_pango: false,
-        description: ROption::RSome("Test match for the plugin API demo".into()),
-        id: ROption::RNone, // The ID can be used for identifying the match later, is not required
-    }]
-    .into()
+fn get_matches(input: RString, config: &Config) -> RVec<Match> {
+    if !input.starts_with(&config.prefix) {
+        RVec::new()
+    } else {
+        vec![Match {
+            title: input.trim_start_matches(&config.prefix).into(),
+            description: ROption::RSome("Search your Repositories".into()),
+            use_pango: false,
+            icon: ROption::RNone,
+            id: ROption::RNone,
+        }]
+        .into()
+    }
 }
 
 #[handler]
-fn handler(selection: Match) -> HandleResult {
-    // Handle the selected match and return how anyrun should proceed
+fn handler(selection: Match, config: &Config) -> HandleResult {
+    if let Err(why) = Command::new("sh")
+        .arg("-c")
+        .arg(format!(
+            "xdg-open https://{}",
+            &encode(&selection.title.to_string())
+        ))
+        .spawn()
+    {
+        println!("Failed to perform websearch: {}", why);
+    }
+
     HandleResult::Close
 }
-
