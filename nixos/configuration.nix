b519e4a3c3c646f4cccd3b3e5d736fbb9903ee80
Sebastian Klähn <scoreplayer2000@gmail.com>
2025-05-22 14:38:49 +0200

    add extra substitutors

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -20,6 +20,13 @@
     settings = {
       experimental-features = "nix-command flakes";
       auto-optimise-store = true;
+      builders-use-substitutes = true;
+      substituters = ["https://hyprland.cachix.org" "https://anyrun.cachix.org"];
+      trusted-substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+      ];
     };
   };
 
