ccee58e796412d545d09eed00f3327ded3b29708
Septias <scoreplayer2000@gmail.com>
2025-07-04 12:56:25 +0200

    own cachix cache

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -21,11 +21,13 @@
       experimental-features = "nix-command flakes";
       auto-optimise-store = true;
       builders-use-substitutes = true;
-      substituters = ["https://hyprland.cachix.org" "https://anyrun.cachix.org"];
-      trusted-substituters = ["https://hyprland.cachix.org"];
+      substituters = [
+        "https://hyprland.cachix.org"
+        "https://septias.cachix.org"
+      ];
       trusted-public-keys = [
+        "septias.cachix.org-1:49NwrE90/oHelsmAb1Ib7madbZtXoDzACAycBDWQ8Sw="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
       ];
     };
   };
