00161355f153c9d546f3f4041796cd000d618b1c
Sebastian Klähn <info@sebastian-klaehn.de>
2025-10-06 09:39:43 +0900

    kitty

--- home-manager/modules/kitty.nix
+++ home-manager/modules/kitty.nix
@@ -9,24 +9,20 @@
     };
     settings = {
       confirm_os_window_close = 0;
-      remember_window_size = true;
-      initial_window_width = 640;
-      initial_window_height = 1080;
       hide_window_decorations = true;
       tab_bar_style = "powerline";
       enabled_layouts = "tall,stack";
       background_opacity = 0.9;
     };
     keybindings = {
-      "ctrl+t" = "launch --type=os-window --cwd=current";
-      "ctrl+shift+f" = "launch --type=overlay --stdin-source=@screen_scrollback ${pkgs.fzf}/bin/fzf --no-sort --no-mouse --exact -i";
-      "ctrl+shift+t" = "launch --type=tab --cwd=current";
-      "ctrl+shift+n" = "next_window";
       "ctrl+q" = "close_window";
-      "kitty_mod+/" = "launch";
-      "ctrl+e" = "launch --type=overlay --cwd=current hx .";
+      "ctrl+t" = "launch --type=os-window --cwd=current";
+      "ctrl+shift+t" = "launch --type=tab --cwd=current";
+      "ctrl+shift+f" = "launch --type=overlay --stdin-source=@screen_scrollback ${pkgs.fzf}/bin/fzf --no-sort --no-mouse --exact -i";
+      "ctrl+e" = "launch --type=os-window --cwd=current --copy-env hx .";
+      "ctrl+w" = "launch --type=overlay --cwd=current --copy-env"; # testing
       "ctrl+l" = "launch --type=overlay --cwd=current lazygit";
-      "ctrl+shift+e" = "combine : launch --type=background --hold=no --cwd=current codium .: close_window";
+      "ctrl+shift+e" = "combine : launch --type=background --hold=no --copy-env --cwd=current codium .: close_window";
     };
   };
 
