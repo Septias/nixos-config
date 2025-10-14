d21a2c44a52c7045f5c7e062c2b30ade602fc95d
Sebastian Klähn <info@sebastian-klaehn.de>
2025-10-14 16:24:11 +0900

    NII monitor 2

--- hosts/laptop/home-manager.nix
+++ hosts/laptop/home-manager.nix
@@ -3,6 +3,7 @@
     settings.monitor = [
       "eDP-1,1920x1200,0x0,1"
       "desc:Eizo Nanao Corporation EV2785 0x03015951, 2560x1440, 0x-1440,1"
+      "DP-1, 2560x1440, 0x-1440,1"
       "HDMI-A-1,2560x1440, 0x-1440,1"
       ", preferred, auto, 1, mirror, eDP-1"
     ];
