668a370dad617865f8220de08e5dce90ea0bdd50
Sebastian Klähn <info@sebastian-klaehn.de>
2026-01-19 13:24:54 +0900

    adjust vpn

--- nixos/vpn.nix
+++ nixos/vpn.nix
@@ -3,6 +3,7 @@
     allowedUDPPorts = [51820]; # Clients and peers can use the same port, see listenport
   };
   # Enable WireGuard
+  networking.firewall.checkReversePath = "loose";
   networking.wireguard.interfaces = {
     # "wg0" is the network interface name. You can name the interface arbitrarily.
     wg0 = {
@@ -15,13 +16,13 @@
       # Note: The private key can also be included inline via the privateKey option,
       # but this makes the private key world-readable; thus, using privateKeyFile is
       # recommended.
-      privateKeyFile = "~/wireguard-keys";
+      privateKeyFile = "/home/septias/wireguard-keys/private";
 
       peers = [
         # For a client configuration, one peer entry for the server will suffice.
         {
           # Public key of the server (not a file path).
-          publicKey = "{server public key}";
+          publicKey = "Kx3hgE5QuA36AOCpMdwlXs63n0DffCV5iC5CvFEie2E=";
 
           # Forward all the traffic via VPN.
           allowedIPs = ["0.0.0.0/0"];
@@ -29,7 +30,7 @@
           #allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
 
           # Set this to the server IP and port.
-          endpoint = "{server ip}:51820"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
+          endpoint = "91.99.26.215:51820"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
 
           # Send keepalives every 25 seconds. Important to keep NAT tables alive.
           persistentKeepalive = 25;
