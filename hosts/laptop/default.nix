00760f9c8a3ee505c0e1e7e183fb686776b56903
Sebastian Klähn <info@sebastian-klaehn.de>
2026-01-19 09:41:29 +0900

    laptop powermanagement

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -1,4 +1,8 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [./hardware-configuration.nix];
 
   networking = {
@@ -15,6 +19,10 @@
     upower.enable = true;
     thermald.enable = true;
     logind.settings.Login.HandleLidSwitch = "suspend";
+    udev.extraRules = ''
+      SUBSYSTEM=="usb", ATTR{idVendor}=="046d", ATTR{idProduct}=="c09d", TEST=="power/control", ATTR{power/control}="on"
+      SUBSYSTEM=="usb", ATTR{idVendor}=="046d", ATTR{idProduct}=="c246", TEST=="power/control", ATTR{power/control}="on"
+    '';
     tlp = {
       enable = true;
       settings = {
@@ -24,9 +32,9 @@
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
-        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MIN_PERF_ON_AC = 1;
         CPU_MAX_PERF_ON_AC = 100;
-        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MIN_PERF_ON_BAT = 1;
         CPU_MAX_PERF_ON_BAT = 20;
 
         START_CHARGE_THRESH_BAT0 = 40;
